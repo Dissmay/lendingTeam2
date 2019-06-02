3bd8fb6325244332e2a29cfa3565a4524d4d3593
Dissmay <Dissmay21@gmail.com>
2019-06-02 18:50:31 +0300

    Сделал все задания по 13 уроку.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
- window.addEventListener('load' , function(){
+ $(function(){
 
  	var mobileBtn = document.querySelector('.mobile-menu');
  	var menu = document.querySelector('.menu');
@@ -10,53 +10,120 @@
  	menu.addEventListener('click', function(){
  		menu.classList.remove('isOpened');
  		mobileBtn.classList.remove('closer');
-
  	});
 
- 	var active = document.querySelector('.active');
- 	var snacks = document.querySelector('#snacks');
-  	var sweets = document.querySelector('#sweets');
-  	var coffee = document.querySelector('#coffee');
- 	var coffeeTab = document.querySelector('#coffeeTab');
- 	var sweetsTab = document.querySelector('#sweetsTab');
- 	var snacksTab = document.querySelector('#snacksTab');
- 	
-	coffeeTab.addEventListener('click', function(e){
-			e.preventDefault();
-	 		coffeeTab.classList.add('active');
-	 		sweetsTab.classList.remove('active');
-	 		snacksTab.classList.remove('active');
-	 		coffee.classList.add('active');
-	 		sweets.classList.remove('active');
-	 		snacks.classList.remove('active');
-	 	});
+ 		/* Jquery */
 
-
- 	sweetsTab.addEventListener('click', function(e){
- 		e.preventDefault();
- 		sweetsTab.classList.add('active');
- 		coffeeTab.classList.remove('active');
- 		snacksTab.classList.remove('active');
- 		sweets.classList.add('active');
- 		coffee.classList.remove('active');
- 		snacks.classList.remove('active');
-
- 	});
-
+ 	 var btnModals = $('.btn_modal');
  	
 
- 	snacksTab.addEventListener('click', function(e){
- 		e.preventDefault();
- 		snacksTab.classList.add('active');
- 		sweetsTab.classList.remove('active');
- 		coffeeTab.classList.remove('active');
- 		snacks.classList.add('active');
- 		sweets.classList.remove('active');
- 		coffee.classList.remove('active');
- 	});
+ 		btnModals.on('click', function(event){
+ 			event.preventDefault();
+ 			var target = $(event.target);
+ 			var link = target.attr('href');
+
+ 			var content = $(link);
+ 			content.addClass('active');
+
+ 			var container = content.parents('.modal-container');
+ 			container.removeClass('d-n');
+ 		});
+ 		var infoBtnModals = $('.info__btn');
+ 		infoBtnModals.on('click', function(event){
+ 			event.preventDefault();
+ 			var target = $(event.target);
+ 			var link = target.attr('href');
+
+ 			var content = $(link);
+ 			content.addClass('active');
+
+ 			var container = content.parents('.modal-container');
+ 			container.removeClass('d-n');
+
+ 		});
 
 
 
 
+ 		/* кнопка клоузер */
 
-});
+ 		var closer = $('.closer');
+
+ 		closer.on('click', function(event){
+ 			var target = $(event.target);
+ 			var container = target.parents('.modal-container');
+ 			container.addClass('d-n');
+ 			var active = $('.modal-container .active');
+ 			active.removeClass('active');
+ 		});
+
+ 		$('.slider').slick({
+ 			prevArrow: $('.slider-container__btn_back'),
+ 			nextArrow: $('.slider-container__btn_next')
+
+ 		});
+
+
+ 		/* Tabs */
+
+
+ 		var tabHeads = $('.product-tab__link');
+ 		 tabHeads.on('click', function(event){
+ 		 	event.preventDefault();
+ 		 	 var target = $(event.target);
+ 		 	 var link = target.attr('href');
+
+ 		 	 var content = $(link);
+ 		 	 var active = $('.product-tab .active, .product-content .active');
+ 		 	 active.removeClass('active');
+ 		 	 target.parent().addClass('active');
+ 		 	 content.addClass('active');
+ 		 })
+
+
+ 		/* Easy scroll */
+
+ 		var navLinks = $('.navigation__a');
+
+ 		navLinks.on('click', function(event){
+ 			event.preventDefault();
+ 			var target = $(event.target);
+ 			var href = target.attr('href');
+
+ 			var elemToScroll = $(href);
+ 			$('html, body').animate({
+ 				scrollTop: elemToScroll.offset().top
+ 			}, 500)
+ 		});
+
+
+
+
+ 		/* кнопка возврата на верх страницы. сделал ее появление*/
+
+ 		$(window).on('scroll', function(event){
+ 			var target = $(event.target);
+ 			var scrollTop = target.scrollTop();
+ 			var btn = $('.btn-menu');
+ 			if(scrollTop > 800){
+ 				btn.addClass('scroll')
+ 			} else{
+ 				btn.removeClass('scroll')
+ 			} 			
+
+ 		}); 
+
+ 		/* Кнопка возврата на верх страницы, сделал чтобы она работала */
+ 		var btnMenu = $('.btn-menu');
+ 		btnMenu.on('click', function(event){
+ 			event.preventDefault();
+ 			var target = $(event.target);
+ 			var href = target.attr('href');
+ 			var menuBtn = $(href);
+ 			$('html, body').animate({
+ 				scrollTop: menuBtn.offset().top -50
+ 			}, 500)
+ 		})
+
+
+})
